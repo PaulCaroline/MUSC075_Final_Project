24e9c3c572dba51c1ea6ca24a200aca900ac9762
Paul Caroline <paulemmit@gmail.com>
2021-05-06 04:43:37 -0400

    update servo plucking motion

--- src/servo_control.ino/servo_control.ino.ino
+++ src/servo_control.ino/servo_control.ino.ino
@@ -2,24 +2,38 @@
 
 ////////////////////////////////////////////////////////////////
 // Author: Paul Caroline
-// Date: 28 Apr. 2021
+// Date: 06 May 2021
 // Final Project: Servo Control
 // MUSC 075 - Musical Interfaces in Robotics
 /////////////////////////////////////////////////////////////////
 
+
+#include <Servo.h>
+
 // Define pins for capacitive touch
-const int touchReadPin1 = 15;
-const int touchReadPin2 = 16;
-const int touchReadPin3 = 17;
-const int touchReadPin4 = 18;
+const int input1 = 15;
+const int input2 = 16;
+const int input3 = 17;
+const int input4 = 18;
 
-// Define touch read values
-int touchVal1;
-int touchVal2;
-int touchVal3;
-int touchVal4;
+/*
+* Set a threshold value to govern the difference between two touch signals
+* that would be large enough to signify a stopped string.
+*/
+const int threshold = 50;
 
-int touchData[] = {touchVal1, touchVal2, touchVal3, touchVal4};
+// Define current touch read values
+int curr1;
+int curr2;
+int curr3;
+int curr4;
+
+// Define previous touch read values
+int  prev1;
+int  prev2;
+int  prev3;
+int  prev4;
+
 
 // Define our servos
 Servo servo1;
@@ -27,33 +41,10 @@ Servo servo2;
 Servo servo3;
 Servo servo4;
 
-Servo servos[] = {servo1, servo2, servo3, servo4};
-
-// Define angle constraints
-const int minAngle = 0;
-const int maxAngle = 50;
-
-// Angle increment
-int angleStep = 30;
-
-// Variables for servo position in degrees
-int angle1 = 1;
-int angle2 = 1;
-int angle3 = 1;
-int angle4 = 1;
-
-int angles[] = {angle1, angle2, angle3, angle4};
-
-// Define touch on/off
-bool touching1 = false;
-bool touching2 = false;
-bool touching3 = false;
-bool touching4 = false; 
-
-bool touchOnOff[] = {touching1, touching2, touching3, touching4};
 
 void setup() {
   Serial.begin(9600);
+
   
   // Define servo signal inputs
   servo1.attach(3);
@@ -66,76 +57,58 @@ void setup() {
   servo2.write(1);
   servo3.write(1);
   servo4.write(1);
+
+  // Initialize touch read values
+  curr1 = touchRead(input1);
+  curr2 = touchRead(input2);
+  curr3 = touchRead(input3);
+  curr4 = touchRead(input4);
 }
 
 void loop() {
+  // Save previous touch values
+  prev1 = curr1;
+  prev2 = curr2;
+  prev3 = curr3;
+  prev4 = curr4;
 
-  // Read touch values
-  touchVal1 = touchRead(touchReadPin1);
-  touchData[0] = touchVal1;
-  touchVal2 = touchRead(touchReadPin2);
-  touchData[1] = touchVal2;
-  touchVal3 = touchRead(touchReadPin3);
-  touchData[2] = touchVal3;
-  touchVal4 = touchRead(touchReadPin4);
-  touchData[3] = touchVal4;
-  
-  // Print integer outputs
-  Serial.print("G string: ");
-  Serial.print(touchVal4);
-  Serial.println("\n");
+  // Read in new touch values
+  curr1 = touchRead(input1);
+  curr2 = touchRead(input2);
+  curr3 = touchRead(input3);
+  curr4 = touchRead(input4);
 
-  Serial.print("C string: ");
-  Serial.print(touchVal3);
-  Serial.println("\n");
+  // Check signal differences and pluck stopped strings
+  if (curr1 - prev1 > threshold) {
+    Serial.println("Triggered string 1");
 
-  Serial.print("E string: ");
-  Serial.print(touchVal2);
-  Serial.println("\n");
+    servo1.write(45);
+    delay(100);
+    servo1.write(0);
+   }
 
-  Serial.print("A string: ");
-  Serial.print(touchVal1);
-  Serial.println("\n");
+  if (curr2 - prev2 > threshold) {
+    Serial.println("Triggered string 2");
 
+    servo2.write(45);
+    delay( 100);
+    servo2.write(0);
+   }
 
-  // Determine from touch values if the string is stopped
-  for (int k = 0; k < 4; k++) {
-    if (touchData[k] >= 1100) {
-      touchOnOff[k] = true;
-      Serial.print(k);
-      Serial.print(": ");
-      Serial.print(touchData[k]);
-      Serial.println();
-      delay(1000);
-    } else {
-      touchOnOff[k] = false;    
-    }
-  }
+  if (curr3 - prev3 > threshold) {
+    Serial.println("Triggered string 3");
 
-  // Calculate next servo moves
-  for (int i = 0; i < 4; i++) {
-    if (touchOnOff[i]) {
+    servo3.write(45);
+    delay(100);
+    servo3.write(0);
+   }
 
-      // change the angle for the next iteration of the loop
-      angles[i] = angles[i] + angleStep;
+  if (curr4 - prev4 > threshold) {
+    Serial.println("Triggered string 4");
 
-      // Reverse the direction
-      if (angles[i] >= maxAngle) {
-        angleStep = - angleStep;
-      }
-
-      if (angles[i] <= minAngle) {
-        angleStep = - angleStep;
-      }
-    }
-  }
-
-  // Pluck the triggered strings
-  for (int j = 0; j < 4; j++) {
-    if (touchOnOff[j]) {
-      servos[j].write(angles[j]);
-    }
-  }
-
-  delay(100);
+    servo3.write(45);
+    delay(100);
+    servo4.write(0);
+   }
+      
 }
